02f448cebf3fee09b73e0b9951098be9b313440e
Steve Nelson <stephennelson1@charter.net>
2025-02-18 11:19:01 -0600

    Initial commit

---
+++ Minesweeper.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <SDL3/SDL.h>
+
+
+int main(int argc, char* argv[])
+{
+    //Declare null window pointer
+    SDL_Window* Window = nullptr;
+
+    //Try init video, if unsucessful print error message
+    if(SDL_Init(SDL_INIT_VIDEO))
+    {
+        
+        //Create window
+        Window = SDL_CreateWindow("Minesweeper", 640, 480, SDL_WINDOW_RESIZABLE);
+        //Ensure window was created
+        if(Window)
+        {
+            //Update window surface to actually see the window
+            SDL_UpdateWindowSurface(Window);
+            //Wait 2 seconds
+            SDL_Delay(2000);
+        }
+        else
+        {
+            std::cout << "Failed to create SDL window: " << SDL_GetError() << "\n";
+        }
+    }
+    else
+    {
+        std::cout << "SDL_Init_Video Error: " << SDL_GetError() << "\n";
+    }
+
+    //Destroy window
+    //Quit SDL
+    SDL_DestroyWindow(Window);
+    SDL_Quit();    
+  
+    return 0;
+}
