18924e9eb522a0f22281bc380abfd411508075b6
Steve Nelson <stephennelson1@charter.net>
2025-02-20 15:14:44 -0600

    Border Tiles
    Added drawing of border tiles

--- Source/Minesweeper.cpp
+++ Source/Minesweeper.cpp
@@ -6,7 +6,7 @@ constexpr int TILE_SIZE = 23;
 constexpr int SCREEN_WIDTH = 640;
 constexpr int SCREEN_HEIGHT = 480;
 
-constexpr int GRID_X = TILE_SIZE * 15;
+constexpr int GRID_X = TILE_SIZE * 10;
 constexpr int GRID_Y = TILE_SIZE * 15;
 
 int DIFFICULTY = 20;
@@ -16,7 +16,7 @@ SDL_Window* Window = nullptr;
 SDL_Surface* WSurface = nullptr;
 SDL_Surface* TileSurface = nullptr; //Covered tile image
 SDL_Surface* TileTypeSurface[11] = {nullptr}; //Uncovered tile images
-SDL_Surface* BorderSurface[4] = {nullptr}; //Border tiles
+SDL_Surface* BorderSurface = nullptr; //Border tiles
 TileMap* MainMap = nullptr;
 
 
@@ -27,11 +27,24 @@ enum : uint8_t
     INDEX_FLAG = 10
 };
 
+enum : uint8_t
+{
+    BORDER_T = 1,
+    BORDER_TL,
+    BORDER_L,
+    BORDER_BL,
+    BORDER_B,
+    BORDER_BR,
+    BORDER_R,
+    BORDER_TR
+};
+
 
 bool Init();
 bool LoadMedia();
 bool LoadFromType(int Type);
 void Close();
+void DrawBorderTiles(int w, int h, int Width, int Height, SDL_Rect D2);
 bool EndGame(bool GameWon);
 
 SDL_Rect ConstructRect(int Height, int Width, int PosX, int PosY)
@@ -107,6 +120,18 @@ int main(int argc, char* argv[])
                     }
                 }
                  SDL_FillRect(WSurface, nullptr, SDL_MapRGB(WSurface->format, 0xB0, 0xB0, 0xB0)); //Draw background
+                //Draw Border
+                int BWidth = Width + 2;
+                int BHeight = Height + 2;
+                for(int bw = 0; bw < BWidth; bw++)
+                {
+                    for(int bh = 0; bh < BHeight; bh++)
+                    {
+                        SDL_Rect Destination = ConstructRect(TILE_SIZE, TILE_SIZE, bw*TILE_SIZE + (OFFSET_X - TILE_SIZE), bh*TILE_SIZE + (OFFSET_Y - TILE_SIZE));
+                    }
+                }
+
+                
                 //Draw array of tiles
                 for(int w = 0; w < Width; w++)
                 {
@@ -137,6 +162,10 @@ int main(int argc, char* argv[])
                             }
                             SDL_BlitSurface(TileTypeSurface[I], nullptr, WSurface, &Destination);
                         }
+
+                        //Draw Border Tiles
+                        DrawBorderTiles(w, h, Width, Height, Destination);
+                       
                     }
                 }
                 SDL_UpdateWindowSurface(Window);
@@ -189,6 +218,12 @@ bool LoadMedia()
         std::cout << "Failed to load Tile.bmp" << SDL_GetError() << "\n";
         return false;
     }
+    BorderSurface = SDL_LoadBMP("Resources/Border_Tileset.bmp");
+    if(TileSurface == nullptr)
+    {
+        std::cout << "Failed to load Border_Tileset.bmp" << SDL_GetError() << "\n";
+        return false;
+    }
     for(int i = 0; i < 11; i++)
     {
         LoadFromType(i);
@@ -216,15 +251,68 @@ void Close()
     delete(MainMap);
     SDL_DestroyWindow(Window);
     SDL_FreeSurface(TileSurface);
+    SDL_FreeSurface(BorderSurface);
     for(auto Surface : TileTypeSurface)
     {
         SDL_FreeSurface(Surface);
     }
-    for(auto Surface : BorderSurface)
-    {
-        SDL_FreeSurface(Surface);
-    }
     SDL_Quit();
+}
+
+void DrawBorderTiles(int w, int h, int Width, int Height, SDL_Rect Destination)
+{
+     if(w == 0) // Left side
+    {
+        SDL_Rect D2 = Destination;
+        D2.x -= TILE_SIZE;
+        SDL_Rect Source = ConstructRect(TILE_SIZE, TILE_SIZE, BORDER_L * TILE_SIZE, 0);
+        SDL_BlitSurface(BorderSurface, &Source, WSurface, &D2);
+        if(h == 0)//Top Left corner
+        {
+            D2.y -= TILE_SIZE;
+            Source.x = BORDER_TL * TILE_SIZE;
+            SDL_BlitSurface(BorderSurface, &Source, WSurface, &D2);
+        }
+        else if(h == Height - 1) //Bottom Left Corner
+        {
+            D2.y += TILE_SIZE;
+            Source.x = BORDER_BL * TILE_SIZE;
+            SDL_BlitSurface(BorderSurface, &Source, WSurface, &D2);
+        }
+    }
+    else if (w == Width - 1)//Right side
+    {
+        SDL_Rect D2 = Destination;
+        D2.x += TILE_SIZE;
+        SDL_Rect Source = ConstructRect(TILE_SIZE, TILE_SIZE, BORDER_R * TILE_SIZE, 0);
+        SDL_BlitSurface(BorderSurface, &Source, WSurface, &D2);
+        if(h == 0)//Top Right corner
+        {
+            D2.y -= TILE_SIZE;
+            Source.x = BORDER_TR * TILE_SIZE;
+            SDL_BlitSurface(BorderSurface, &Source, WSurface, &D2);
+        }
+        else if(h == Height - 1) //Bottom Right Corner
+        {
+            D2.y += TILE_SIZE;
+            Source.x = BORDER_BR * TILE_SIZE;
+            SDL_BlitSurface(BorderSurface, &Source, WSurface, &D2);
+        }
+    }
+    if(h == 0)//Top
+    {
+        SDL_Rect D2 = Destination;
+        D2.y -= TILE_SIZE;
+        SDL_Rect Source = ConstructRect(TILE_SIZE, TILE_SIZE, BORDER_T * TILE_SIZE, 0);
+        SDL_BlitSurface(BorderSurface, &Source, WSurface, &D2);
+    }
+    else if(h == Height - 1)//Bottom
+    {
+        SDL_Rect D2 = Destination;
+        D2.y += TILE_SIZE;
+        SDL_Rect Source = ConstructRect(TILE_SIZE, TILE_SIZE, BORDER_B * TILE_SIZE, 0);
+        SDL_BlitSurface(BorderSurface, &Source, WSurface, &D2);
+    }
 }
 
 bool EndGame(bool GameWon)
